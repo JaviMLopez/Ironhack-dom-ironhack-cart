a1b1fdde9270ffedcaa5c7c94002fe6de9c15f1f
Lopez Javier <20203@columbushs.com>
2019-12-09 13:45:44 -0500

    wip

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -3,11 +3,25 @@ var $calc = document.getElementById('calc');
 
 function updateSubtot($product) {
   // Iteration 1.1
-  let unitPrice = $product.querySelector('.product .pu span').innerHTML;
-  let unitQuantity = $product.querySelector('.product label input').value;
-  
+  let unitPrice = Number($product.querySelector('.product .pu span').innerHTML);
+  let unitQuantity = Number($product.querySelector('.product label input').value);
 
+  let subtotal = unitPrice * unitQuantity;
+  
+  $product.querySelector('.subtot span').innerHTML = subtotal;
+  return subtotal;
+}
 function calcAll() {
   // Iteration 1.2
+  let productCollection = $cart.getElementByClass("product");
+  let $product;
+  let subtotal = 0;
+
+  for(i=0; i < productCollection.length; i++){
+    $product = productCollection[i];
+    subtotal += updateSubtot($product);
+  }
+  updateSubtot();
+
 }
 $calc.onclick = calcAll;
