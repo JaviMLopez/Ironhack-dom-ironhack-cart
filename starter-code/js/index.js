7809fcd46df0aa06312b54055847dbb84738722e
Lopez Javier <20203@columbushs.com>
2019-12-10 09:39:50 -0500

    wip

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,6 +1,8 @@
 var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 
+
+
 function updateSubtot($product) {
   // Iteration 1.1
   let unitPrice = Number($product.querySelector('.product .pu span').innerHTML);
@@ -13,15 +15,36 @@ function updateSubtot($product) {
 }
 function calcAll() {
   // Iteration 1.2
-  let productCollection = $cart.getElementByClass("product");
+  let productCollection = $cart.getElementsByClassName("product");
   let $product;
-  let subtotal = 0;
+  let sumtotal = 0;
 
   for(i=0; i < productCollection.length; i++){
     $product = productCollection[i];
-    subtotal += updateSubtot($product);
+    sumtotal += updateSubtot($product);
   }
-  updateSubtot();
+  
+document.querySelector("h2 span").innerHTML = sumtotal;
 
+updateSubtot();
 }
 $calc.onclick = calcAll;
+
+let removeCartButtons = document.getElementsByClassName("btn btn-delete");
+for (i=0; i<removeCartButtons.length; i++){
+  let button = removeCartButtons[i];
+  button.addEventListener('click', function(event) {
+    let buttonClicked = event.target;
+    buttonClicked.parentElement.parentElement.remove();
+  })
+}
+
+let createButton = document.getElementById("create");
+for (i=0; i<createButton.length; i++){
+  let button = createButton[i];
+  button.addEventListener('clicked', addToCartClicked)
+}
+
+function addToCartClicked(event){
+  
+}
