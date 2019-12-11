740fd38647a26952f696f58b494d3a343474b951
Lopez Javier <20203@columbushs.com>
2019-12-11 12:44:12 -0500

    It's Over (Done)

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -30,6 +30,7 @@ updateSubtot();
 }
 $calc.onclick = calcAll;
 
+function boomer(){
 let removeCartButtons = document.getElementsByClassName("btn btn-delete");
 for (i=0; i<removeCartButtons.length; i++){
   let button = removeCartButtons[i];
@@ -38,13 +39,52 @@ for (i=0; i<removeCartButtons.length; i++){
     buttonClicked.parentElement.parentElement.remove();
   })
 }
+}
+boomer();
 
 let createButton = document.getElementById("create");
-for (i=0; i<createButton.length; i++){
-  let button = createButton[i];
-  button.addEventListener('clicked', addToCartClicked)
-}
+let button = createButton;
+button.addEventListener('click', function(element){
+    let button = event.target;
+    let title = document.getElementsByClassName("new")[0].getElementsByTagName("input")[0].value;
+    let number = document.getElementsByClassName("new")[0].getElementsByTagName("input")[1].value;
 
-function addToCartClicked(event){
+    let cartRow = document.createElement("tr");
+    cartRow.classList.add("product");
+    let cartItems = document.getElementsByClassName("product")[0];
+    let cartRowContents = `
+    <td class="name">
+          <span>${title}</span>
+        </td>
+    
+        <td class="pu">
+          $<span>${number}</span>
+        </td>
+    
+        <td class="qty">
+          <label>
+            <input type="number" value="0" min="0">
+          </label>
+        </td>
+    
+        <td class="subtot">
+          $<span>0</span>
+        </td>
+    
+        <td class="rm">
+          <button class="btn btn-delete">Delete</button>
+        </td>`
+    cartRow.innerHTML = cartRowContents;
   
-}
+    $cart.append(cartRow);
+    boomer();
+  })
+
+
+// function addToCartClicked(event){
+//   let button = event.target;
+//   // let shopItem = document.getElementsByClassName("new")[0];
+//   var title = document.getElementsByClassName("new")[0].getElementsByTagName("input")[0].value;
+//   // let title = shopItem.getElementsByTagName("input")[0].value; 
+//   console.log(title);
+// }
